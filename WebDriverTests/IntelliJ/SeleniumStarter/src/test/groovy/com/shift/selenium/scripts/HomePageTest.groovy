6c716dbe78388517345a45cb82ba1aa316d01bdb
kenichiro.ota <kenichiro.ota@shiftinc.jp>
2014-01-22 15:55:20 +0900

    convert from JUnit to Geb

--- WebDriverTests/IntelliJ/SeleniumStarter/src/test/groovy/com/shift/selenium/scripts/HomePageTest.groovy
+++ WebDriverTests/IntelliJ/SeleniumStarter/src/test/groovy/com/shift/selenium/scripts/HomePageTest.groovy
@@ -1,34 +1,33 @@
-package com.shift.selenium.scripts;
+package com.shift.selenium.scripts
 
-
-import selenium.pages.HomePage;
-import selenium.pages.SecondPage;
-import org.junit.After;
-import org.junit.Before;
-import org.junit.Test;
-import org.openqa.selenium.Alert;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.Wait;
-import org.openqa.selenium.support.ui.WebDriverWait;
-
-import static org.hamcrest.CoreMatchers.is;
-import static org.junit.Assert.assertThat;
+import org.openqa.selenium.Alert
+import org.openqa.selenium.WebDriver
+import org.openqa.selenium.firefox.FirefoxDriver
+import org.openqa.selenium.support.ui.ExpectedConditions
+import org.openqa.selenium.support.ui.WebDriverWait
+import selenium.pages.HomePage
+import selenium.pages.SecondPage
+import spock.lang.Specification
 
 /**
- * Created with IntelliJ IDEA.
- * User: lei.wang
- * Date: 13/11/03
- * Time: 12:36
- * To change this template use File | Settings | File Templates.
+ * Created by kenichiro_ota on 2014/01/22.
  */
-public class HomePageTest {
-    def driver
-    def homePage
+class HomePageTest extends Specification {
+    WebDriver driver
+    HomePage homePage
 
-    @Test
-    public void testElementsExistenceAndDefaultValues() {
+    void setup() {
+        driver = new FirefoxDriver()
+        homePage = (HomePage) new HomePage(driver).get()
+        homePage.initializeWebElement()
+    }
+
+    void cleanup() {
+        driver.quit()
+    }
+
+    def "ElementsExistenceAndDefaultValues"() {
+        expect:
         assert homePage.isTextPresent("Seleniumテスト用ファイル")
         assert homePage.getInputElementsByType("checkbox").size() == 3
         assert !homePage.getFemaleRadioboxStatus()
@@ -36,44 +35,40 @@ public class HomePageTest {
         assert homePage.getSelectedCountry() == "日本"
     }
 
-    @Test
-    public void testControlLogic() {
+
+    def "ControlLogic"() {
+        when:
         // check alert
         homePage.enterName("Kevin Wang")
         homePage.fireNameAlert()
+
+        then:
         verifyAlertWithText("名前: Kevin Wang")
     }
 
-    @Test
-    public void testCalculation() {
+    def "Calculation"() {
+        when:
         homePage.calculate(2.0, 3.1)
-        def wait = new WebDriverWait(driver, 10)
+        def wait = new WebDriverWait(driver, 10L)
+
+        then:
         assert homePage.getCalculationResult() == "5.1"
     }
 
-    @Test
     public void testPageTransition() {
+        when:
         SecondPage secondPage = homePage.moveToNextPage()
+
+        then:
         assert secondPage.isTextPresent("Seleniumテスト用ファイル02")
     }
 
-    private void verifyAlertWithText(String text) {
-        def wait = new WebDriverWait(driver, 2)
+    void verifyAlertWithText(String text) {
+        def wait = new WebDriverWait(driver, 2L)
         wait.until(ExpectedConditions.alertIsPresent())
         Alert alert = driver.switchTo().alert()
         assert alert.getText() == text
         alert.accept()
     }
 
-    @Before
-    public void setUp() {
-        driver = new FirefoxDriver();
-        homePage = (HomePage) new HomePage(driver).get();
-        homePage.initializeWebElement();
-    }
-
-    @After
-    public void tearDown() {
-        driver.quit();
-    }
 }
